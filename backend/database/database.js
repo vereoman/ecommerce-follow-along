741118c29a6dc0e567758bd2389effa96289ce1c
Arman <contact.armankar@gmail.com>
2025-01-17 22:22:37 +0530

    completed milestone 3

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-function connectDB() {
+async function connectDB() {
     try {
-        const conn = mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, async function () {
     try {
         await connectDB();
