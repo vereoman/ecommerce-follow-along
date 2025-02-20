d549ee7175ac743fb451166cb4edaee17fd3aba2
Arman <contact.armankar@gmail.com>
2025-02-20 19:39:00 +0530

    Completed Assignment 35

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
 const cors = require("cors");
-
 const userRouter = require("./routes/user.routes");
 const productRouter = require("./routes/product.routes");
 const cartRouter = require("./routes/cart.routes");
@@ -11,7 +10,6 @@ const orderRouter = require("./routes/order.routes");
 const paymentRouter = require("./routes/payment.routes");
 
 const app = express();
-
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,5 +23,11 @@ app.use("/payments", paymentRouter);
 
 app.use((req, res) => res.status(404).json({ message: "Route not found" }));
 
-const PORT = process.env.PORT || 5000;
-connectDB().then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)));
+connectDB();
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
